891bbbdfad8cbe83191ea355a1750f18ec1100dc
ArcturV <smoug2001@mail.ru>
2019-11-19 16:26:43 +0300

    Initial commit.

---
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.ForeColor = System.Drawing.Color.ForestGreen;
+            this.groupBox1.Location = new System.Drawing.Point(16, 60);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(162, 100);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Choose action";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(7, 63);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(89, 27);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Decode";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(7, 30);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(89, 27);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Encode";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(253, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(286, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Enter letter which will be decode :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.ForeColor = System.Drawing.Color.ForestGreen;
+            this.textBox1.Location = new System.Drawing.Point(257, 96);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(401, 30);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(253, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(182, 23);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Enter the path to file :";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Black;
+            this.textBox2.ForeColor = System.Drawing.Color.ForestGreen;
+            this.textBox2.Location = new System.Drawing.Point(257, 203);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(401, 30);
+            this.textBox2.TabIndex = 5;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.BackColor = System.Drawing.Color.Black;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 467);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Location = new System.Drawing.Point(571, 363);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 33);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button_OnClick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(253, 259);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(223, 23);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Encoding degree(1/2/4/8):";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.Black;
+            this.textBox3.ForeColor = System.Drawing.Color.ForestGreen;
+            this.textBox3.Location = new System.Drawing.Point(257, 304);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 30);
+            this.textBox3.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(477, 157);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(122, 23);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Letter length:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.Black;
+            this.textBox4.ForeColor = System.Drawing.Color.ForestGreen;
+            this.textBox4.Location = new System.Drawing.Point(481, 203);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 30);
+            this.textBox4.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 489);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Bahnschrift", 14F);
+            this.ForeColor = System.Drawing.Color.ForestGreen;
+            this.Name = "Form1";
+            this.Text = "Cipher";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        void radioButton_CheckedChanged(object sender, System.EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                this.label2.Location = new System.Drawing.Point(253, 60);
+                this.label2.Text = "Enter letter which will be decode :";
+                this.label3.Location = new System.Drawing.Point(253,157);
+                this.textBox2.Location = new System.Drawing.Point(257, 203);
+                this.textBox1.Location = new System.Drawing.Point(257,96);
+                this.label4.Location = new System.Drawing.Point(253, 259);
+                this.textBox3.Location = new System.Drawing.Point(257, 304);
+                this.button1.Text = "Encode";
+                this.toolStripStatusLabel1.Text = "Ready to encode";
+                this.label5.Visible = false;
+                this.textBox4.Visible = false;
+                this.textBox4.Clear();
+                this.textBox2.Clear();
+                this.textBox3.Clear();
+                this.textBox1.Clear();
+            }
+            else if (radioButton2.Checked)
+            {
+                this.label5.Visible = true;
+                this.textBox4.Visible = true;
+                this.label2.Text = "Decoded letter :";
+                this.label2.Location = new System.Drawing.Point(253, 259); // на место label4
+                this.textBox1.Location = new System.Drawing.Point(257, 304);
+                this.label3.Location = new System.Drawing.Point(253, 60);
+                this.textBox2.Location = new System.Drawing.Point(257, 96);
+                this.label4.Location = new System.Drawing.Point(253, 157);
+                this.textBox4.Clear();
+                this.textBox2.Clear();
+                this.textBox3.Clear();
+                this.textBox1.Clear();
+                this.textBox3.Location = new System.Drawing.Point(257, 203);
+                this.button1.Text = "Decode";
+                this.toolStripStatusLabel1.Text = "Ready to decode";
+            }
+        }
+        
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+    
+}
+
---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = getBlockquote();
+        }
+        void button_OnClick(object sender, System.EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                textBox2.ForeColor = Color.ForestGreen;
+                byte[] encodingLetterBytes = Encoding.GetEncoding(1251).GetBytes(textBox1.Text);
+                //byte degree = (byte)(2);
+                byte degree = Convert.ToByte(textBox3.Text);
+                byte bits = (byte)(8);
+                FileStream fileWithEncode = new FileStream(textBox2.Text.Replace(".", "(1)."), FileMode.Create);
+                fileWithEncode.Close();
+                byte[] imageBytes = File.ReadAllBytes(textBox2.Text);
+                if (imageBytes.Length == 0)
+                {
+                    File.Delete(Directory.GetCurrentDirectory() + "\\" + textBox2.Text);
+                    textBox2.ForeColor = Color.Red;
+                }
+                else
+                {
+                    for (int i = 54, counter = 0; i < 54 + (encodingLetterBytes.Length) * (bits / degree); i = i + bits / degree, counter++)
+                    {
+                        for (int j = i, z = 0; z < bits / degree; j++, z++)
+                        {
+
+                            imageBytes[j] = (byte)(imageBytes[j] & getImageMask(degree));// очищаем последниее разряды
+                            byte tempTextBits = (byte)((encodingLetterBytes[counter] & getTextMask(degree)) >> bits - degree);// берем биты из байта символа кодируемоего сообщения
+                            imageBytes[j] |= tempTextBits;
+                            encodingLetterBytes[counter] <<= degree;
+
+                        }
+                    }
+                    File.WriteAllBytes(textBox2.Text.Replace(".", "(1)."), imageBytes);
+                    toolStripStatusLabel1.Text = "Encoded complete";
+                }
+                //кажется работает
+            }
+            else if (radioButton2.Checked)
+            {
+                textBox2.ForeColor = Color.ForestGreen;
+                byte[] byteOnDecode = File.ReadAllBytes(textBox2.Text);
+                if (byteOnDecode.Length == 0)
+                {
+                    File.Delete(Directory.GetCurrentDirectory()+"\\"+textBox2.Text);
+                    textBox2.ForeColor = Color.Red;
+                }
+                else
+                {
+                    int symbolsQuantity = Convert.ToInt16(textBox4.Text);
+                    byte degree = Convert.ToByte(textBox3.Text);
+                    byte bits = (byte)(8);
+                    byte[] reverseOnReturn = new byte[symbolsQuantity];
+                    for (int i = 54, k = 0; i < 54 + symbolsQuantity * (bits / degree); i = i + bits / degree, k++)
+                    {
+                        byte symbol = 0;
+                        for (int j = i, z = 0; z < bits / degree; j++, z++)
+                        {
+
+                            byte workByte = byteOnDecode[j];
+                            workByte &= getReverseMask(degree);
+                            symbol <<= degree;
+                            symbol |= workByte;
+
+                        }
+                        reverseOnReturn[k] = symbol;
+                    }
+                    string s = Encoding.GetEncoding(1251).GetString(reverseOnReturn);
+                    textBox1.Text = s;
+                }
+            }// и это тоже работает(вроде)
+        }
+        static byte getImageMask(byte degree)
+        {
+            return (byte)(255 >> degree << degree);
+        }
+        static byte getTextMask(byte degree)
+        {
+            return (byte)(255 >> (byte)(8) - degree << (byte)(8) - degree);
+
+        }
+        static byte getReverseMask(byte degree)
+        {
+            return (byte)(255 >> 8 - degree);
+        }
+        public string getBlockquote()
+        {
+            string s = "";
+            System.Random rnd = new System.Random();
+            switch (rnd.Next(0, 2))
+            {
+                case 0:
+                    s  = "Privacy and secrecy are not the same thing...";
+                    break;
+                case 1:
+                    s = "Privacy is the ability to choose what information about yourself to open to the world...";
+                    break;
+                case 2:
+                    s = "Encryption is the main way to protect your privacy...";
+                    break;
+            }
+            return s;
+        }
+    };
+}
+
